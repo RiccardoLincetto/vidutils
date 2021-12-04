ffecb7f4ae5d07ce3cbd68513109b5e2d33b15a4
Riccardo Lincetto <riccardo.lincetto@gmail.com>
2021-12-04 23:25:26 +0100

    Make modules visible

--- vidutils/__init__.py
+++ vidutils/__init__.py
@@ -1,4 +1,7 @@
-from . import *
+from vidutils import procs
+from vidutils import script
+from vidutils import utils
+from vidutils import video
 
 __version__ = "0.3.0"
 __author__ = 'Riccardo Lincetto'
