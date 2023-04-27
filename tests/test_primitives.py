c723ec07578dd486e0281af53825005c801c3c10
Riccardo Lincetto <37301469+RiccardoLincetto@users.noreply.github.com>
2023-04-27 10:08:14 +0000

    wip(writer): setup video writer from reader info

--- tests/test_primitives.py
+++ tests/test_primitives.py
@@ -1,8 +1,11 @@
+from unittest.mock import MagicMock
+
+import cv2
 import numpy as np
 import pytest
 
 from tests import settings
-from vidutils.primitives import Frame
+from vidutils.primitives import Frame, Reader, Writer
 
 
 class TestFrameCreation:
@@ -34,3 +37,27 @@ class TestFrameProperties:
     def test_channels(self, shape: tuple[int, ...]):
         frame = Frame(np.zeros(shape))
         assert frame.channels == (shape[2] if len(shape) == 3 else 1)
+
+
+class TestWriter:
+    @pytest.fixture
+    def mock_capture(self) -> MagicMock:
+        mock_capture = MagicMock(spec=cv2.VideoCapture)
+        mock_capture.get.side_effect = (30, 100, 100)  # fps, width, height
+        return mock_capture
+
+    @pytest.fixture
+    def mock_reader(self) -> MagicMock:
+        mock_reader = MagicMock(spec=Reader)
+        mock_reader.get.side_effect = (30, 100, 100)  # fps, width, height
+        return mock_reader
+
+    def test_init_with_capture(self, mock_capture: cv2.VideoCapture):
+        writer = Writer("path/to/output/video.mp4", mock_capture)
+        assert isinstance(writer, Writer)
+        assert isinstance(writer, cv2.VideoWriter)
+
+    def test_init_with_reader(self, mock_reader: Reader):
+        writer = Writer("path/to/output/video.mp4", mock_reader)
+        assert isinstance(writer, Writer)
+        assert isinstance(writer, cv2.VideoWriter)
